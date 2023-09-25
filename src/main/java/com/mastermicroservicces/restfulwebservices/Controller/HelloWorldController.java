a57b96caa3f527ebb9d1a820f690cd61028d8290
praveenay <praveenya923@gmail.com>
2023-09-25 11:50:46 +0530

    Providing Path Variable in rest api

--- src/main/java/com/mastermicroservicces/restfulwebservices/Controller/HelloWorldController.java
+++ src/main/java/com/mastermicroservicces/restfulwebservices/Controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.mastermicroservicces.restfulwebservices.Controller;
 
 import com.mastermicroservicces.restfulwebservices.Bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
     }
 
+    //providing path variable
+    @GetMapping(path ="/hello-world/path-variable/{name}" )
+    public HelloWorldBean helloWorldPathVarialble(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
 
 }
